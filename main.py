7d2ba883dbfc3f741b148f96404ab02dbd3d2e5b
Brandon <juhkim90@gmail.com>
2021-02-26 03:46:23 +0000

    add the multiplication game

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 seconds = time.time()
 print (seconds)
 
-countdown = 10
+countdown = 5
 
 while (countdown >= 0):
 	print (countdown)
@@ -12,6 +12,41 @@ while (countdown >= 0):
 	time.sleep(1)
 else:
 	print ("Vrmmmmm!")
+
+# Multiplication Game
+import random
+
+print ("Welcome to the Multiplication Game\n")
+
+# Generates two random numbers in variables.
+rand1 = random.randint(1,9)
+rand2 = random.randint(1,9)
+
+# Countdown
+print ("Here is the Question!")
+countdown = 3
+while (countdown > 0):
+	print (countdown)
+	countdown -= 1
+	time.sleep(1)
+else:
+  print ("\n\n")
+
+# Measure Initial Time
+ti = time.time()
+# Ask user the question
+question = int(input(str(rand1) + " x " + str(rand2) + ": "))
+# Measure Final Time
+tf = time.time()
+
+# Check the answer
+# Correct answer, print out correct and elapsed time
+if (question == rand1 * rand2):
+  print ("Correct")
+  print ("Elapsed Time: " + str(tf - ti))
+else:
+  print ("Nice Try... Wrong answer")
+
 
 
 
